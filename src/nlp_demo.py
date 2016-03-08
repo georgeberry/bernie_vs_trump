0e677539e0362407d4907cef406dd58375a64d33
Tom Davidson <trd54@cornell.edu>
2016-03-07 21:15:46 -0500

    updated demo

--- src/nlp_demo.py
+++ src/nlp_demo.py
@@ -1,24 +1,41 @@
-####This is a simple example to show what the different tools do
+import string
+import nltk
+import sklearn
+from sklearn.feature_extraction.text import TfidfVectorizer
 
+T = "I would like to offer my sincerest condolences to the Scalia family after the passing of Justice Scalia. Justice Scalia was a remarkable person and a brilliant Supreme Court Justice, one of the best of all time. His career was defined by his reverence for the Constitution and his legacy of protecting Americans' most cherished freedoms. He was a Justice who did not believe in legislating from the bench and he is a person whom I held in the highest regard and will always greatly respect his intelligence and conviction to uphold the Constitution of our country. My thoughts and prayers are with his family during this time."
+B = "U.S. Sen. Bernie Sanders issued the following statement on Saturday ont he passing of U.S. Supreme Court Justice Antonin Scalia. While I differed with Justice Scalia's views and jurisprudence, he was a brilliant, colorful and outspoken member of the Supreme Court. My thoughts and prayers are with his family and his colleagues on the court who mourn his passing."
 
+def lower(s):
+    s = s.lower()
+    return s
 
-    import string
-    import nltk
+def remove_punct(s):
+    punct = set(string.punctuation)
+    s = ''.join(ch for ch in s if ch not in punct)
+    return s
 
+def tokens(s):
+    tokens = nltk.word_tokenize(s)
+    return tokens
 
-    T = "I would like to offer my sincerest condolences to the Scalia family after the passing of Justice Scalia. Justice Scalia was a remarkable person and a brilliant Supreme Court Justice, one of the best of all time. His career was defined by his reverence for the Constitution and his legacy of protecting Americans’ most cherished freedoms. He was a Justice who did not believe in legislating from the bench and he is a person whom I held in the highest regard and will always greatly respect his intelligence and conviction to uphold the Constitution of our country. My thoughts and prayers are with his family during this time
-    B = "DENVER – U.S. Sen. Bernie Sanders issued the following statement on Saturday on the passing of U.S. Supreme Court Justice Antonin Scalia.While I differed with Justice Scalia’s views and jurisprudence, he was a brilliant, colorful and outspoken member of the Supreme Court. My thoughts and prayers are with his family and his colleagues on the court who mourn his passing.”
+stopwords = nltk.corpus.stopwords.words("english")
 
-
-def Tokenizer(sentence):
-    #Coverts uppercase to lower
-    sentence = sentence.lower()
-    #Removes all punctuation
-    sentence = sentence.translate(string.maketrans("",""), string.punctuation)
-    #Converts sentence to tokens
-    tokens = nltk.word_tokenize(sentence)
-    #Removes stopword
+def words(tokens):
     words = [word for word in tokens if not word in stopwords]
-    #Stems the tokens using the PorterStemmer
+    return words
+
+def stems(words):
+    stemmer = nltk.stem.porter.PorterStemmer()
     stems = [stemmer.stem(word) for word in words]
     return stems
+
+if __name__ == '__main__':
+    T1 = lower(T)
+    T2 = remove_punct(T1)
+    B1 = lower(B)
+    B2 = remove_punct(B1)
+    vectorizer = TfidfVectorizer(tokenizer=tokens, stop_words=stopwords)
+    tfidf = vectorizer.fit_transform([T2,B2])
+    print tfidf
+    print ((tfidf * tfidf.T))[0,1]
